989450d33ea202079f6ffd599fe6b6f54b1d68c8
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-08-26 14:18:08 +0530

    user route part done

--- api/routes/users.js
+++ api/routes/users.js
@@ -12,7 +12,7 @@ router.put("/:id", verify, async (req, res) => {
         }
 
         try {
-            const updateUser = await User.findByIdAndUpdate(req.params.id, { $set: req.body });
+            const updateUser = await User.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
 
             res.status(200).json(updateUser);
         } catch (error) {
@@ -25,12 +25,50 @@ router.put("/:id", verify, async (req, res) => {
 })
 
 // Delete User
-
+router.delete("/:id", verify, async (req, res) => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+        try {
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("Deleted User");
+        } catch (error) {
+            res.status(500).json()
+        }
+    }
+    else {
+        res.status(403).json("You can delete only tour account")
+    }
+})
 
 // Get User
+router.get("/find/:id", verify, async (req, res) => {
 
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...info } = user._doc
+        res.status(200).json(info);
+    } catch (error) {
+        res.status(500).json()
+    }
+
+
+})
 
 // Get All User
+router.get("/", verify, async (req, res) => {
+
+    const query = req.query.new;
+    if (req.user.isAdmin) {
+        try {
+            const users = query ? await User.find().limit(10) : await User.find()
+            res.status(200).json(users);
+        } catch (error) {
+            res.status(500).json()
+        }
+    }
+    else {
+        res.status(403).json("You are not allowed to see all user")
+    }
+})
 
 
 // Get User Stats
